65af418dc08b54feb8300563919fb0a9b36d70f5
PonceGL <poncianogl@hotmail.com>
2026-01-23 14:50:16 -0600

    Initial Commit

---
+++ Signum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Signum
+//
+//  Created by Ponciano Guevara Lozano on 23/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Signum/SignumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignumApp.swift
+//  Signum
+//
+//  Created by Ponciano Guevara Lozano on 23/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SignumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
